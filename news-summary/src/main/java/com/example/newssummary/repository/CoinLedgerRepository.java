a68757265484727549a7b6cd178e55e0d67c2629
yeonw00 <mrk8056@gmail.com>
2025-10-24 20:57:30 +0900

    CoinLedgerRepository 수정

--- news-summary/src/main/java/com/example/newssummary/repository/CoinLedgerRepository.java
+++ news-summary/src/main/java/com/example/newssummary/repository/CoinLedgerRepository.java
@@ -1,9 +1,16 @@
 package com.example.newssummary.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.newssummary.dao.CoinLedger;
+import com.example.newssummary.dao.LedgerType;
 
-public interface CoinLedgerRepository extends JpaRepository<CoinLedger, Long>{
-
+public interface CoinLedgerRepository extends JpaRepository<CoinLedger, Long> {
+    Page<CoinLedger> findByUserIdAndType(Long userId, LedgerType type, Pageable pageable);
+    Page<CoinLedger> findByUserId(Long userId, Pageable pageable);
+    Optional<CoinLedger> findByRequestId(String requestId); // 멱등 처리용
 }
