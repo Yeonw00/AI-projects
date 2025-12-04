924f5fcb9f1a0d6767d935dad1c0d1dcf651cf66
yeonw00 <mrk8056@gmail.com>
2025-12-04 21:30:56 +0900

    payment_order DB에 paymentKey 추가

--- news-summary/src/main/java/com/example/newssummary/repository/PaymentOrderRepository.java
+++ news-summary/src/main/java/com/example/newssummary/repository/PaymentOrderRepository.java
@@ -1,11 +1,15 @@
 package com.example.newssummary.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.newssummary.dao.OrderStatus;
 import com.example.newssummary.dao.PaymentOrder;
 
 public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long>{
 	Optional<PaymentOrder> findByOrderUid(String OrderUid);
+	
+	List<PaymentOrder> findByPaymentKeyIsNullAndStatus(OrderStatus status);
 }
---
+++ news-summary/src/main/resources/db/migration/V8__add_payment_key_to_payment_order.sql
@@ -0,0 +1,2 @@
+ALTER TABLE payment_order
+	ADD COLUMN payment_key VARCHAR(255);
